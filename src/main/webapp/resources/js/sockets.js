02b642ba8224b7eb2ce3633be1a389f2fe5feb1b
Jakub Chalupa <chaluja7@fel.cvut.cz>
2015-02-01 17:08:24 +0100

    socket endpoint port on openshift

--- src/main/webapp/resources/js/sockets.js
+++ src/main/webapp/resources/js/sockets.js
@@ -1,5 +1,10 @@
-var webSocket =
-    new WebSocket('ws://' + document.location.host + document.location.pathname.replace(/[^/]*$/, '') + 'messagewebsocket');
+var currentHost = document.location.host;
+
+if(currentHost.indexOf('localhost') == -1 && currentHost.indexOf('127.0.0.1') == -1) {
+    currentHost = currentHost.replace(':8080', ':8000');
+}
+
+var webSocket = new WebSocket('ws://' + currentHost + document.location.pathname.replace(/[^/]*$/, '') + 'messagewebsocket');
 
 webSocket.onerror = function(event) {
     onError(event)
